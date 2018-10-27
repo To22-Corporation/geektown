475c2a906a587e90d3471c6f5fc9e2070a1bdf5f
Kohei <mannnnsu446@icloud.com>
2018-10-27 14:47:51 +0900

    db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2018_10_18_063124) do
 
-  create_table "people", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.integer "age"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -37,4 +30,5 @@ ActiveRecord::Schema.define(version: 2018_10_18_063124) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
