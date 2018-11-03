c2627bbf5cffe62c3f88bfea765d1606685912ef
Masahide Ishida <masa.ishi.right@gmail.com>
2018-11-03 23:57:43 +0900

    top

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'users#index'
+  root to: 'home#index'
   devise_for :users
   resources :users
   resources :contacts do
     post :confirm, action: :confirm_new, on: :new
   end
-  get '/home', to: 'home#index'
 end
