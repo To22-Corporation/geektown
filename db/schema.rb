c0fd95c0a0cb5db8eaab39faaef7fd36fd0ec7da
oky-123 <oky123.ia@gmail.com>
2018-11-13 23:29:21 +0900

    Fix db connection bug

--- db/schema.rb
+++ db/schema.rb
@@ -49,13 +49,6 @@ ActiveRecord::Schema.define(version: 2018_11_02_051826) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "people", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.integer "age"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
